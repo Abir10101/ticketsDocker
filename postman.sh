0de4e57182a0271169d5e3e9887e7c09b2f7d9da
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2022-12-19 09:09:34 +0530

    main

--- postman.sh
+++ postman.sh
@@ -1,13 +1,13 @@
-curl --location --request POST 'http://localhost:5000/register' \
+curl --location --request POST 'http://172.105.36.103:5000/register' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "username":"test5",
-    "password": "test5",
-    "name":"test5"
+    "username":"test1",
+    "password": "test1",
+    "name":"test1"
 }'
 
 
-curl --location --request POST 'http://localhost:5000/login' \
+curl --location --request POST 'http://172.105.36.103:5000/login' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "username":"test5",
@@ -15,21 +15,21 @@ curl --location --request POST 'http://localhost:5000/login' \
 }'
 
 
-curl --location --request POST 'http://localhost:5000/logout' \
+curl --location --request POST 'http://172.105.36.103:5000/logout' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjQ1OTYxNjMsImlhdCI6MTY2NDUwOTc2Mywic3ViIjozMywic2VjcmV0IjoidlB2d1JIVkZWR0hLRWciLCJleHBpcnkiOiIxNjY0NTc2MzYzIn0.Q154ZQI3b5c_A4IA9zRz6MfkU6E9IpXAtEvbMyev1X4"
+    "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE1MDM4MjAsImlhdCI6MTY3MTQxNzQyMCwic3ViIjoxLCJzZWNyZXQiOiI5RGlpTGl6RnFYaVVMUSIsImV4cGlyeSI6IjE2NzE1MDM4MjAifQ.s1lkIqZvOSenlOWRqQaNJQE58oF4xQoFzcMFf8gtWGM"
 }'
 
 
-curl --location --request GET 'http://localhost:5000/tickets' \
+curl --location --request GET 'http://172.105.36.103:5000/tickets' \
 --header 'Content-Type: application/json' \
---header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzAzODM0NjcsImlhdCI6MTY3MDI5NzA2Nywic3ViIjoxLCJzZWNyZXQiOiJiYUZ1VXpUSnl1TXNtQSIsImV4cGlyeSI6IjE2NzAzODM0NjcifQ.r56Rydm7sJhXOYDm6-gARokjGJbOTmhSeQMK6yG_hx8'
+--header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE1MDM4MjAsImlhdCI6MTY3MTQxNzQyMCwic3ViIjoxLCJzZWNyZXQiOiI5RGlpTGl6RnFYaVVMUSIsImV4cGlyeSI6IjE2NzE1MDM4MjAifQ.s1lkIqZvOSenlOWRqQaNJQE58oF4xQoFzcMFf8gtWGM'
 
 
-curl --location --request POST 'http://localhost:5000/tickets' \
+curl --location --request POST 'http://172.105.36.103:5000/tickets' \
 --header 'Content-Type: application/json' \
---header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzAzODM0NjcsImlhdCI6MTY3MDI5NzA2Nywic3ViIjoxLCJzZWNyZXQiOiJiYUZ1VXpUSnl1TXNtQSIsImV4cGlyeSI6IjE2NzAzODM0NjcifQ.r56Rydm7sJhXOYDm6-gARokjGJbOTmhSeQMK6yG_hx8' \
+--header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE1MDM4MjAsImlhdCI6MTY3MTQxNzQyMCwic3ViIjoxLCJzZWNyZXQiOiI5RGlpTGl6RnFYaVVMUSIsImV4cGlyeSI6IjE2NzE1MDM4MjAifQ.s1lkIqZvOSenlOWRqQaNJQE58oF4xQoFzcMFf8gtWGM' \
 --data-raw '{
     "code":"vfd-1234",
     "description": "asdf",
@@ -37,7 +37,7 @@ curl --location --request POST 'http://localhost:5000/tickets' \
 }'
 
 
-curl --location --request PATCH 'http://localhost:5000/tickets' \
+curl --location --request PATCH 'http://172.105.36.103:5000/tickets' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjQ2ODQyODIsImlhdCI6MTY2NDU5Nzg4Miwic3ViIjozMywic2VjcmV0IjoiMXlCeXdHSE1LSW5jUVEiLCJleHBpcnkiOiIxNjY0NjY0NDgyIn0.1gaC5UypBJ2-8TszPfccy5GRSzqV3KKhPicMZMiw5-I' \
 --data-raw '{
@@ -48,7 +48,7 @@ curl --location --request PATCH 'http://localhost:5000/tickets' \
 }'
 
 
-curl --location --request POST 'http://localhost:5000/branches' \
+curl --location --request POST 'http://172.105.36.103:5000/branches' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjQ2ODQyODIsImlhdCI6MTY2NDU5Nzg4Miwic3ViIjozMywic2VjcmV0IjoiMXlCeXdHSE1LSW5jUVEiLCJleHBpcnkiOiIxNjY0NjY0NDgyIn0.1gaC5UypBJ2-8TszPfccy5GRSzqV3KKhPicMZMiw5-I' \
 --data-raw '{
@@ -58,7 +58,7 @@ curl --location --request POST 'http://localhost:5000/branches' \
 }'
 
 
-curl --location --request GET 'http://localhost:5000/branches' \
+curl --location --request GET 'http://172.105.36.103:5000/branches' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjQ2ODQyODIsImlhdCI6MTY2NDU5Nzg4Miwic3ViIjozMywic2VjcmV0IjoiMXlCeXdHSE1LSW5jUVEiLCJleHBpcnkiOiIxNjY0NjY0NDgyIn0.1gaC5UypBJ2-8TszPfccy5GRSzqV3KKhPicMZMiw5-I' \
 --data-raw '{
